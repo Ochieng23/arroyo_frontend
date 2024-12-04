ea814873b17d788cbd68159bd067b14d48b6da16
paul <ochiengpaul001@gmail.com>
2024-12-04 19:21:56 +0300

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Arroyo Coming Soon</h1>
+      <p className="mt-4 text-lg text-center">
+        We are working hard to bring you something amazing. Stay tuned!
+      </p>
+    </div>
+  );
+}
